a5e845a438c4b0ccd71de97ea60e0e8399c71b34
carlosvldz <me@carlosvldz.com>
2021-03-29 00:00:01 -0600

    change text to spanish

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ io.on('connection', socket => {
         socket.join(user.room);
 
         // Welcome current user
-        socket.emit('message', formatMessage(botName, 'Welcome to Evo-IQ Chat!'));
+        socket.emit('message', formatMessage(botName, 'Bienvenido a Evo-IQ!'));
 
         // Broadcast when a user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} se ha unido a la sala.`));
 
         // Send users and room info
         io.to(user.room).emit('roomUsers', {
@@ -47,7 +47,7 @@ io.on('connection', socket => {
         const user = userLeave(socket.id);
 
         if(user) {
-            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} ha salido de la sala.`));
 
         // Send users and room info
         io.to(user.room).emit('roomUsers', {
